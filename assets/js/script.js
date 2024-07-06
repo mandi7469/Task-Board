cc7cacf4279239d7a58d92b0d6cb78658dba0735
mandi7469 <mandi7469@aol.com>
2024-07-06 12:43:08 -0400

    added starter files and folders

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+// Retrieve tasks and nextId from localStorage
+let taskList = JSON.parse(localStorage.getItem("tasks"));
+let nextId = JSON.parse(localStorage.getItem("nextId"));
+
+// Todo: create a function to generate a unique task id
+function generateTaskId() {
+
+}
+
+// Todo: create a function to create a task card
+function createTaskCard(task) {
+
+}
+
+// Todo: create a function to render the task list and make cards draggable
+function renderTaskList() {
+
+}
+
+// Todo: create a function to handle adding a new task
+function handleAddTask(event){
+
+}
+
+// Todo: create a function to handle deleting a task
+function handleDeleteTask(event){
+
+}
+
+// Todo: create a function to handle dropping a task into a new status lane
+function handleDrop(event, ui) {
+
+}
+
+// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+$(document).ready(function () {
+
+});
